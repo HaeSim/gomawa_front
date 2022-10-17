c242e409a4377661b0a4bdd92d55c26a420d1f73
정보통신-안철진 <cheoljin.an@lotte.net>
2022-10-17 19:45:17 +0900

    feat.adding section scrolling

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 
 import Layout from 'components/layout/Layout';
 import Landing from 'components/Landing';
+import Contents from 'components/Contents';
 
 const Home: NextPage = () => {
   return (
@@ -15,6 +16,7 @@ const Home: NextPage = () => {
 
       <Layout>
         <Landing />
+        <Contents />
       </Layout>
     </div>
   );
