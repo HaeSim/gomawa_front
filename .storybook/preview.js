fd511e763751367376b90b21c7fbd436e216ebb2
chojs <jjcs735@naver.com>
2022-10-18 23:28:27 +0900

    fix: 스토리북 emotion theme 이슈 수정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,16 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+import { ThemeProvider } from '@emotion/react';
+import { Global } from '@emotion/react';
+import globalStyles from '../styles/globalStyles';
+import theme from '../styles/theme';
+
+// emotion 관련 이슈
+// https://howdy-blog-v2.vercel.app/storybook/setting-emotion
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyles} />
+      <Story />
+    </ThemeProvider>
+  ),
+];
